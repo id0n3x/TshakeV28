9fe440bb8df6a6733ed10f95f4bf1532c8501ffa
id0n3x <80592269+id0n3x@users.noreply.github.com>
2021-03-15 23:22:57 +0300

    Update all.py

--- handlers/all.py
+++ handlers/all.py
@@ -51,7 +51,7 @@ def allGP(client, message,redis):
         Bot("sendMessage",{"chat_id":chatID,"text":r.userNocc,"reply_to_message_id":message.message_id,"parse_mode":"html"})
 
     if re.search(c.sors,text):
-      kb = InlineKeyboardMarkup([[InlineKeyboardButton("قناه السورس 📢", url="t.me/calmapubg")],[InlineKeyboardButton("تواصل السورس 💬", url="t.me/calmapubg")],[InlineKeyboardButton("شروحات السورس 📑", url="t.me/calmapubg")]])
+      kb = InlineKeyboardMarkup([[InlineKeyboardButton("قناه السورس 📢", url="t.me/zx_xx")],[InlineKeyboardButton("تواصل السورس 💬", url="t.me/zx_xx")],[InlineKeyboardButton("شروحات السورس 📑", url="t.me/zx_xx")]])
       Botuser = client.get_me().username
       Bot("sendMessage",{"chat_id":chatID,"text":r.sors.format("@"+Botuser),"disable_web_page_preview":True,"reply_to_message_id":message.message_id,"parse_mode":"markdown","reply_markup":kb})
     
